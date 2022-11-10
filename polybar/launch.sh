86da66b6c07cda995d6b8be25db1d7398fe6335a
Connor Saila <csaila@live.com>
2022-11-10 12:38:23 -0500

    polybar launch script updates

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall polybar
+killall -q polybar
 
 # Launch polybar
-polybar -q left
-polybar -q right
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar left 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar2.log & disown
 
-fi
+echo "Bars launched..."
